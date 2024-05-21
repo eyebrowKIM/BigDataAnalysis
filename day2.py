8fc55c0c4e40e5ef9047ddc761c51b03c50afa02
Junsub Kim <albert1226@g.skku.edu>
2024-05-21 19:11:09 +0900

    add

--- day2.py
+++ day2.py
@@ -27,6 +27,71 @@ df['new_price'] = df['item_price'].str[1:].astype('float')
 print(df[df['new_price']<=5])
 print(len(df[df['new_price']<=5]))
 # %%
+#문제 26
 new_df = df[df['item_name']=='Chicken Salad Bowl']
 new_df.reset_index(drop=True)
+# %%
+#문제 27
+new_df = df[(df['item_name']=='Chicken Salad Bowl') & (df['new_price']<=9)]
+print(new_df)
+# %%
+#문제 28
+new_df = df.sort_values(by='new_price').reset_index(drop=True)
+print(new_df)
+# %%
+'''못 푼 문제'''
+#문제 29
+new_df = df.loc[df.item_name.str.contains('Chips')]
+print(new_df)
+# %%
+'''못 푼 문제'''
+#문제 30
+df.iloc[:,::2]
+# %%
+#문제 31
+new_df = df.sort_values(by='new_price', ascending=False).reset_index(drop=True)
+print(new_df)
+# %%
+#문제 32
+'''못 푼 문제'''
+Ans = df.loc[(df.item_name =='Steak Salad') | (df.item_name =='Bowl')]
+print(Ans)
+# %%
+#문제 33
+new_df = df.loc[(df.item_name == 'Steak Salad') | (df.item_name == 'Bowl')]
+new_df.drop_duplicates(subset='item_name', keep='first')
+# %%
+#문제 34
+new_df = df.loc[(df.item_name == 'Steak Salad') | (df.item_name == 'Bowl')]
+new_df.drop_duplicates(subset='item_name', keep='last')
+# %%
+#문제 35
+mean = df['new_price'].mean()
+print(df[df['new_price']>=mean])
+# %%
+#문제 36
+df.replace(to_replace='Izze', value='Fizzy Lizzy')
+# %%
+#문제 37
+df['choice_description'].isna().sum()
+# %%
+#문제 38
+'''못 푼 문제'''
+import numpy as np
+df.replace(to_replace=np.NaN, value='Nodata')
+# %%
+#문제 39
+df[df.choice_description.str.contains("Black", na=False)]
+# %%
+#문제 40
+total = len(df.choice_description)
+Veg = df[df.choice_description.str.contains("Vegetables", na=False)]
+length = len(Veg)
+print(total-length)
+# %%
+#문제 41
+df[df.item_name.str.startswith('N')]
+# %%
+#문제 42
+df[df.item_name.str.len() >= 15]
 # %%
